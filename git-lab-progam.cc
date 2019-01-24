b841a4782809046cfb93ef27581215b20d8241e5
Brandon Huff <bh480818@ohio.edu>
2019-01-23 19:18:17 -0500

    git lab file

---
+++ git-lab-progam.cc
@@ -0,0 +1,17 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Brandon Huff
+ *      Date: 1-22-19
+ *      Description: says hello git!
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *arvg[]){
+     cout << "Hello Git!!" << endl;
+     return 0;
+ }
